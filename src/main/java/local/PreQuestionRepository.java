734205195525cddf1df6d18a2796ca0872686dbe
brightsun-leeys <brightsun.leeys@gmail.com>
2020-09-03 00:50:36 +0900

    hahahah a-o...

--- src/main/java/local/PreQuestionRepository.java
+++ src/main/java/local/PreQuestionRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface PreQuestionRepository extends PagingAndSortingRepository<PreQuestion, Long>{
-    List<PreQuestion> findByHospitalId(String HospitalId);
-    PreQuestion findByScreeningId(Long ScreeningId);
+
 }
